f5a6ccc090422ac7623ea21f1a2df29d7037c7a9
ArthurVinicioAlbuquerqueRochadaSilva <vinicioarthur690@gmail.com>
2025-09-22 23:27:42 -0300

    att

--- operações binarias/ex2.py
+++ operações binarias/ex2.py
@@ -1,10 +1,17 @@
 import sys
+
 intvalor = int(input('Digite um número inteiro:'))
+
 if intvalor < 0:
     sys.exit('Digite valor positivo')
+
 intQtbits = intvalor.bit_length()
 print(intQtbits)
+
 binValor = bin(intvalor)[2:]
 print(binValor)
+
 if len(binValor) % 8 != 0:
-    
+    intZeros = 8 - len(binValor) % 8
+binValor = ('0' * intZeros) + binValor
+print(f'Final: {binValor}') 
